e9b0ea5f07f7339be87362e537037c66dc204410
ghifarizk <ghifarizaki000@gmail.com>
2025-02-10 18:07:00 +0700

    commit
    update

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// document.addEventListener('wheel', function(event) {
+//   if (event.ctrlKey) {
+//     event.preventDefault();
+//   }
+// }, { passive: false });
+
+// document.addEventListener('gesturestart', function(event) {
+//   event.preventDefault();
+// });
