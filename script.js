f49ab8786b7e69d52d3c589a8768637875a77233
ghifarizk <ghifarizaki000@gmail.com>
2025-02-11 23:25:26 +0700

    commit
    update

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+document.addEventListener('wheel', function(event) {
+  if (event.ctrlKey) {
+    event.preventDefault();
+  }
+}, { passive: false });
 
-// document.addEventListener('wheel', function(event) {
-//   if (event.ctrlKey) {
-//     event.preventDefault();
-//   }
-// }, { passive: false });
-
-// document.addEventListener('gesturestart', function(event) {
-//   event.preventDefault();
-// });
-
-
+document.addEventListener('gesturestart', function(event) {
+  event.preventDefault();
+});
